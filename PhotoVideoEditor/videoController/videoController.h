d3ca129000bc7d39dca3533f0be0ce95287f94d9
Ashwin R <ashwinr14@gmail.com>
2019-06-23 15:25:23 -0700

    Add shared data

---
+++ PhotoVideoEditor/videoController/videoController.h
@@ -0,0 +1,19 @@
+//
+//  videoController.h
+//  videoController
+//
+//  Created by Ashwin Ramachandran on 6/23/19.
+//  Copyright © 2019 Faris Albalawi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for videoController.
+FOUNDATION_EXPORT double videoControllerVersionNumber;
+
+//! Project version string for videoController.
+FOUNDATION_EXPORT const unsigned char videoControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <videoController/PublicHeader.h>
+
+
